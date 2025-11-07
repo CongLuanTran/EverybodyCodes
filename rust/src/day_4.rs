895478d9c6ca71f63056a347ac44c93c256dcc30
Cong Luan Tran <congluan.tran19@gmail.com>
2025-11-07 19:17:39 +0700

    feat: Add day 4 solutions in Rust

---
+++ rust/src/day_4.rs
@@ -0,0 +1,44 @@
+use crate::utils::{Solution, read_lines, test};
+
+pub struct Day4;
+
+impl Solution for Day4 {
+    type Output = Vec<String>;
+
+    fn get_test(file: std::path::PathBuf) -> Self::Output {
+        read_lines(file).unwrap()
+    }
+
+    fn part1() -> String {
+        let gears = Self::get_test(test(4, 1));
+
+        (2025 * gears.first().unwrap().parse::<u64>().unwrap()
+            / gears.last().unwrap().parse::<u64>().unwrap())
+        .to_string()
+    }
+
+    fn part2() -> String {
+        let gears = Self::get_test(test(4, 2));
+
+        (10_000_000_000_000_f64 * gears.last().unwrap().parse::<f64>().unwrap()
+            / gears.first().unwrap().parse::<f64>().unwrap())
+        .ceil()
+        .to_string()
+    }
+
+    fn part3() -> String {
+        let gears = Self::get_test(test(4, 3));
+        let start: u64 = gears.first().unwrap().parse().unwrap();
+        let end: u64 = gears.last().unwrap().parse().unwrap();
+        let teeth = start
+            * 100
+            * gears[1..gears.len() - 1]
+                .iter()
+                .map(|l| {
+                    let (a, b) = l.split_once("|").unwrap();
+                    b.parse::<u64>().unwrap() / a.parse::<u64>().unwrap()
+                })
+                .product::<u64>();
+        (teeth / end).to_string()
+    }
+}
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,10 +1,12 @@
 use rust::day_1::Day1;
 use rust::day_2::Day2;
 use rust::day_3::Day3;
+use rust::day_4::Day4;
 use rust::utils::Solution;
 
 fn main() {
     Day1::run();
     Day2::run();
     Day3::run();
+    Day4::run();
 }
